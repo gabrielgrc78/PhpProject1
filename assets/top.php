2c79e1fd12734d37d027f3a8cb5ef114b4e2e3bb
Gabriel Garcia <gabrielgrc78@gmail.com>
2020-03-05 00:38:48 -0500

    Update top.php

--- assets/top.php
+++ assets/top.php
@@ -3,11 +3,11 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
+        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
         <link rel="stylesheet" type="text/css" href="assets/css/normalize.css"/>
         <link rel="stylesheet" type="text/css" href="assets/css/main.css"/>
         <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
         <script type="text/javascript" src="assets/js/main.js"></script>
-        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
         <meta name="viewport" content="width=device=width, initial-scale=1"/>
         <meta http-equiv="X-UA-Compatible" content="IE=EDGE; chrome=1"/>
         <title> <?php echo $servername ?> - <?php echo $header ?> </title>
