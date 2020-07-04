a6017839b438f61d4227501b2f6fadb230e347f1
Gabriel Garcia <gabrielgrc78@gmail.com>
2020-07-03 22:36:48 -0400

    Update doregister.php

--- PHP/doregister.php
+++ PHP/doregister.php
@@ -10,26 +10,26 @@ if (isset($_POST['register'])) {
     $ip = getenv("REMOTE_ADDR");
 
     if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        echo "Improper email and username";
-        exit();
+        echo "<p>Improper email and username</p>";
+
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "invalid email";
-        exit();
+        echo "<p>invalid email</p>";
+
     } elseif (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
-        echo "Invalid username!";
-        exit();
+        echo "<p>Invalid username!</p>";
+
     } elseif ($password !== $chkpassword) {
-        echo "password do not match";
-        exit();
+        echo "<p>password do not match</p>";
+
     }  elseif ($birth == null) {
         echo "please enter in a birthday.";
-        exit();
+
     } else {
-        $sql = "SELECT `username` FROM `Clients` WHERE `username`=?";
+        $sql = "SELECT `username` FROM `clients` WHERE `username`=?";
         $stmt = mysqli_stmt_init($connect);
         if (!mysqli_stmt_prepare($stmt, $sql)){
             echo "ERROR = 01: Connection Error";
-            exit();
+
         } else {
             mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_stmt_execute($stmt);
@@ -37,13 +37,13 @@ if (isset($_POST['register'])) {
             $rc = mysqli_stmt_num_rows($stmt);
             if ($rc > 0) {
                 echo "ERROR: Username exist";
-                exit();
+
             } else {
-                $sql = "INSERT INTO `Clients` (`username`,`email`,`password`,`birthdate`,`lastknownip`) VALUES (?,?,?,?,?)";
+                $sql = "INSERT INTO `clients` (`username`,`email`,`password`,`birthdate`,`lastknownip`) VALUES (?,?,?,?,?)";
                 $stmt = mysqli_stmt_init($connect);
                 if (!mysqli_stmt_prepare($stmt, $sql)){
                     echo "ERROR = 02: Connection Error";
-                    exit();
+
                 } else {
                     $securedpass = password_hash($password, PASSWORD_DEFAULT);
                     mysqli_stmt_bind_param($stmt, "sssss", $username,$email, $securedpass, $birth, $ip);
