51772db3efedb8ce9b34cad37ed05511ce3a526e
Gabriel Garcia <gabrielgrc78@gmail.com>
2020-09-01 09:46:17 -0400

    polishing and CSS fixes

--- assets/bottom.php
+++ assets/bottom.php
@@ -5,7 +5,6 @@
             <fieldset>
                 <p>Copyright &copy; <?php echo $servername ?>  <?php echo date("Y"); ?>.</p>
                 <p>Currently on Development Version: <?php echo $version; ?></p>
-                
             </fieldset>
         </footer>
 
