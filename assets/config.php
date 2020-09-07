b3636bcdd1e2cfda3b63fd9b00da31d3671587db
Gabriel Garcia <gabrielgrc78@gmail.com>
2020-09-07 11:08:36 -0400

    improvements (2/2)

--- assets/config.php
+++ assets/config.php
@@ -9,9 +9,9 @@ $host['user'] = "root";
 $host['password'] = "";
 $host['database'] = "YngridHairSalon";
 $host['port'] = "3306";
-//Server//
+//Server info//
 $servername = "Yngrid's Hair Styling";
-$version    = "0.6 (Alpha)";
+$version    = "0.7 (Alpha)";
 //session link//
 $connect = mysqli_connect($host['hostname'],$host['user'],$host['password'],$host['database'],$host['port']) or die ("can't connect to server");
 
--- assets/top.php
+++ assets/top.php
@@ -2,7 +2,6 @@
 session_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>
@@ -20,24 +19,21 @@ session_start();
             <div id="Title">
                <img alt="Yngrid Hair Salon" src="assets/img/LogoMakr_6CVBIV.png"/>
             </div>
-
             <?php if ($header == "Home") {echo "<a class=active href=\"?p=home\">Home</a>";} else {echo "<a href=\"?p=home\">Home</a>";}?>
             <?php
             if (!isset($_SESSION['user'])){
                 if ($header == "Register") {echo "<a class=active href=\"?p=register\">Register</a>";} else {echo "<a href=\"?p=register\">Register</a>";}
                 if ($header == "Login") {echo"<a class=active href=\"?p=login\">Login</a>";} else {echo"<a href=\"?p=login\">Login</a>";}
-                if ($header == "Guest appointment") {echo "<a class=action href=\"?p=ga\">Appointment</a>";} else {echo "<a href=\"?p=ga\">Appointment</a>";}
+                if ($header == "Guest appointment") {echo "<a class=active href=\"?p=ga\">Appointment</a>";} else {echo "<a href=\"?p=ga\">Appointment</a>";}
             } else {
                 if ($_SESSION['admin'] == true){
-                    echo "<a href=\"?p=logout\" > Logout</a>
-                          <a href=\"?p=la\">Appointment</a>
-                          <a href=\"?p=status\">System Status</a>";
+                    echo "<a href=\"?p=logout\" > Logout</a>";
+                    if($header == "Appointment"){echo "<a class=active href=\"?p=la\">Appointment</a>";} else {echo "<a href=\"?p=la\">Appointment</a>";}
+                    if($header == "System Status"){echo "<a class=active href=\"?p=status\">System Status</a>";} else {echo "<a href=\"?p=status\">System Status</a>";}
                 }else {
-                    echo "<a href=\"?p=logout\" > Logout</a>
-                          <a href=\"?p=ga\">Appointment</a>";
+                    echo "<a href=\"?p=logout\" > Logout</a>";
+                    if($header == "Appointment"){echo "<a class=active href=\"?p=la\">Appointment</a>";} else {echo "<a href=\"?p=ga\">Appointment</a>";}
                 }
-            }
-            ?>
+            }?>
             <?PHP if ($header == "Contact") {echo "<a class=active href=\"?p=contact\">Contact Us</a>";} else {echo "<a href=\"?p=contact\">Contact Us</a>";}?>
         </div>
-   
